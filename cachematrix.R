b14def67fb3f754e906daba79ae4902dc2fc5fd0
konradedgar <konrad.zdeb@me.com>
2014-04-23 20:38:11 +0200

    Edits
    Minor edits and typos.

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,11 @@
 ## The file is submitted for the purposes of the R programming course.
-## The function inverses matrix.
-## This file has no purpose expect testing settings in GitHub.
+## The purpose of the function is to inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
   
 }
 
 
-## The function does what it should.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
